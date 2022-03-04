6e1b88efe9621133da6f28d7f2567710911e7ed4
Piotr <straszak.piotr@gmail.com>
2022-03-04 09:05:34 +0100

    TADAAA!

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
 
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from './components/About.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    About,
+    Home
+  },
+  data () {
+    return {
+      componentName: 'Home'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>
+    Home
+  </h1>
+</template>
+
+<script>
+export default {
+  unmounted () {
+    console.log('Home component was unmounted')
+  },
+  activated () {
+    console.log('Home component was activated')
+  },
+  deactivated () {
+    console.log('Home component was deactivated')
+  }
+}
+</script>
